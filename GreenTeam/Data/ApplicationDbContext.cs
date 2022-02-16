25fddc71490408b566e3176b1e231bc6e59bda3d
AnitaWiersma <anitawiersma@live.nl>
2022-02-16 09:34:00 +0100

    Added a custom user class

--- GreenTeam/Data/ApplicationDbContext.cs
+++ GreenTeam/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using GreenTeam.Models;
 
 namespace GreenTeam.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     { 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- GreenTeam/Program.cs
+++ GreenTeam/Program.cs
@@ -1,4 +1,5 @@
 using GreenTeam.Data;
+using GreenTeam.Models;
 using GreenTeam.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -11,8 +12,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<AppUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IGardenService, GardenService>();
