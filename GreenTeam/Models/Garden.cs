5c55d3de10fb45a452eea94e1a7369594a208b34
AnitaWiersma <anitawiersma@live.nl>
2022-03-01 09:31:26 +0100

    GT-70 added more user friendly notifications for entering gardens and patches

--- GreenTeam/Models/Garden.cs
+++ GreenTeam/Models/Garden.cs
@@ -1,9 +1,17 @@
-﻿namespace GreenTeam.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenTeam.Models
 {
     public class Garden
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a name for your garden.")]
+        [StringLength(15)]
+        [RegularExpression(@"[a-zA-Z0-9 \\'\\-]{1,15}", 
+            ErrorMessage = "Please provide a name with the maximum length of 15 characters using only letters, digits" +
+            " and the special characters single quote( ' ) and dash(-).")]
         public string Name { get; set; }
+        [StringLength (35)]
         public string Location { get; set; }
 
         public ICollection<Patch> Patches { get; set; }
--- GreenTeam/Models/Patch.cs
+++ GreenTeam/Models/Patch.cs
@@ -5,9 +5,14 @@ namespace GreenTeam.Models
     public class Patch
     {
         public int Id { get; set; }
+        [StringLength(20)]
         public string Crop { get; set; }
         public int GardenId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter a patchname.")]
+        [StringLength(15)]
+        [RegularExpression(@"[a-zA-Z0-9 \\'\\-]{1,20}",
+            ErrorMessage = "Please provide a patchname with the maximum length of 20 characters using only letters, digits" +
+            " and the special characters single quote( ' ) and dash(-).")]
         public string PatchName { get; set; }
     }
 }
