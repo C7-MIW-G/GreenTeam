b797b05a13cb8adbcfbbb6ea6a8ee985a6d0c22f
Joël Smits <86686910+MisterLifey@users.noreply.github.com>
2022-03-01 16:08:14 +0100

    GT-78 WIP updated database for garden photo's

--- GreenTeam/ViewModels/GardenVM.cs
+++ GreenTeam/ViewModels/GardenVM.cs
@@ -6,6 +6,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public byte[] GardenPhoto { get; set; }
 
         public List<PatchVM> Patches { get; set; }
         public List<AppUserVM> Users { get; set; }
