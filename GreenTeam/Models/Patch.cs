9a5bbd2dd71afcb421068c370c430bd1bd2734be
Bart van Dijk <ba.van.dijk@st.hanze.nl>
2022-03-10 16:59:12 +0100

    GT-91 better edit page design. Editing w/o changing image no longer results in deleting image

--- GreenTeam/Models/Patch.cs
+++ GreenTeam/Models/Patch.cs
@@ -18,10 +18,7 @@ namespace GreenTeam.Models
         public string Crop { get; set; }
         
         public int GardenId { get; set; }
-        [Required(ErrorMessage = "Please enter a name for your patch."),
-            StringLength(20), RegularExpression(@"[a-zA-Z0-9 äëïöü'-]{1,20}",
-            ErrorMessage = "Allowed characters are letters, digits, dash(-) and apostrophe(').")]
-       
+              
         public ICollection<PatchTask> PatchTasks { get; set; }
     }
 }
