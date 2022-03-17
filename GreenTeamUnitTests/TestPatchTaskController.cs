357c17550fce40c6d7858a512134587270e75ac7
Rutger Dijkstra <rutger.red@gmail.com>
2022-03-17 13:07:30 +0100

    added mocking

--- GreenTeamUnitTests/TestPatchTaskController.cs
+++ GreenTeamUnitTests/TestPatchTaskController.cs
@@ -1,39 +1,111 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GreenTeam.Controllers;
+using GreenTeam.Implementations;
+using GreenTeam.Models;
+using GreenTeam.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
 
 namespace GreenTeamUnitTests
 {
-    /*[TestClass]
+    [TestClass]
     public class TestPatchTaskController
     {
+        public Garden createTestGarden()
+        {
+            Garden garden = new Garden()
+            {
+                Id = 42,
+                Name = "TestGarden",
+                Location = "TestLocation",
+                ImageId = 4242,
+                Patches = null
+            };
+
+            return garden;
+        }
+
+        public Patch createTestPatch()
+        {
+            Patch patch = new Patch()
+            {
+                Id = 1,
+                GardenId = 42,
+                Crop = "testCrop",
+                PatchName = "testPatchName",
+
+            };
+
+           return patch;
+        }
+
+        public PatchTask createTestPatchTask()
+        {
+            PatchTask patchTask = new PatchTask()
+            { 
+                Id = 23, 
+                PatchId = 34, 
+                TaskDescription = "testTaskDescription", 
+                TaskName = "testTaskName" 
+            };
+
+            return patchTask;
+        }
+
+        public PatchTaskVM createPatchTaskVM()
+        {
+            PatchTaskVM patchTaskVM = new PatchTaskVM()
+            { 
+                Id = 23, 
+                PatchId = 34, 
+                TaskDescription = "testTaskDescription", 
+                TaskName = "testTaskName" 
+            };
+           
+            return patchTaskVM;
+        }
+
         [TestMethod]
-        public async void PostPatchTaskCreateSuccesfull()
+        public void PostPatchTaskCreateSuccesfull()
         {
             // Arrange
-            PatchTask patchTask = new PatchTask() { Id = 2, PatchId = 1, TaskDescription = "testTaskDescription", TaskName = "testTaskName" };
+            Garden garden = createTestGarden();
+            Patch patch = createTestPatch();         
+            PatchTask patchTask = createTestPatchTask();
+            PatchTaskVM patchTaskVM = createPatchTaskVM();
 
+            var mockIGardenService = new Mock<IGardenService>();
+            mockIGardenService.Setup(x => x.FindById(garden.Id).Result).Returns(garden);
+
+            var mockIPatchService = new Mock<IPatchService>();
+            mockIPatchService.Setup(x => x.FindById(patch.Id).Result).Returns(patch);
+            
             var mockIPatchTaskService = new Mock<IPatchTaskService>();
             mockIPatchTaskService.Setup(q => q.AddPatchTask(patchTask).Result).Returns(patchTask);
             Mapper mapper = new Mapper();
 
-            PatchTasksController controller = new PatchTasksController((IPatchTaskService)mockIPatchTaskService, mapper);
+            var controller = new PatchTasksController(mockIPatchService.Object, 
+                mockIPatchTaskService.Object, mockIGardenService.Object, mapper);
+
+      
 
             // Act
-            var returnedType = await controller.Create(patchTask);
+            var actionResult = controller.Create(patchTask);
+
+            ViewResult actionViewResult = (ViewResult)actionResult.Result;
+            PatchTaskVM actualModel = actionViewResult.Model as PatchTaskVM;
+
 
             // Assert
             //Assert.IsNotNull(returnedType);
-            Assert.AreEqual(patchTask, returnedType);        
-
+            Assert.AreSame(patchTaskVM, actualModel, "Both return same Model View");
+           /* Assert.AreEqual(patchTaskVM, actualModel, "Fuck Testing");
+*/
         }
-
-        public async void PatchTaskEditSuccesfull()
-        {
-            PatchTask patchTask = new PatchTask() { Id = 1, PatchId = 1, TaskDescription = "testTaskDescription", TaskName = "testTaskName" };
-
-            var mockIPatchTaskService = new Mock<IPatchTaskService>();
-            mockIPatchTaskService.Setup(controller => controller.FindById(1).Result).Returns(patchTask);
-            *//*var mockIPatchTaskService = new Mock<IPatchTaskService>();
-            mockIPatchTaskService.Setup(controller => controller.FindById().Result.);*//*
-        }
-    }*/
+    }
 }
